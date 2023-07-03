aed5b48dd4a381af853fd2f26814b3e2bb26287e
unknown <uzochukwuemeka516@gmail.com>
2023-07-03 11:31:32 -0700

    next

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default async function Home() {
         </h1>
         <div className="grid grid-cols-1 gap-y-10  gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
           {products.map((product) => {
-            return <div>
-              <Product key={product.id} product={product} />
+            return <div key={product.id}>
+              <Product  product={product} />
             </div>
           })}
         </div>
